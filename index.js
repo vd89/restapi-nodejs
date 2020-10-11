f6fd1b751e6dad6e1bc25386fe75071a7f0420fb
dixitvora <dixitvora@gmail.com>
2020-10-11 13:20:52 +0530

    Chore: Parse url and get the url

--- index.js
+++ index.js
@@ -2,12 +2,24 @@
   Primary File for API 
 */
 // Dependencies
+const { triggerAsyncId } = require('async_hooks');
 const http = require('http');
 const url = require('url');
 
 // Config the server to response to all request
 const server = http.createServer((req, res) => {
-  return res.end('Server is running \n');
+  // Parse the url
+  const parseUrl = url.parse(req.url, true);
+
+  // Get the path
+  const path = parseUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // send the response
+  res.end('Server is running \n');
+
+  // Log the request/response
+  console.log('Request received on path: ', trimmedPath);
 });
 
 // Start the server
